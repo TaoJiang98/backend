10657b6dcc3df3fb7d59fb0c3a0f4fcbdc5c9ca2
taojiang98 <jerrymchs@gmail.com>
2021-06-07 05:11:36 -0400

    first deploy

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const mongoose = require('mongoose');
 
 // require('dotenv').config();
-const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 
