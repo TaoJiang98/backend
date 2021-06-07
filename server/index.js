0b0badabe30059cea0ed4d3499e0ec67590cf680
taojiang98 <jerrymchs@gmail.com>
2021-06-07 09:03:38 -0400

    2

--- server/index.js
+++ server/index.js
@@ -5,17 +5,20 @@ const path = require('path');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
+const env = "" + process.env.NODE_ENV;
 
 const app = express();
 const port = process.env.PORT || 8081;
 console.log("process.env.PORT: ", process.env.PORT);
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 const middleware = require("./middleware");
 app.use(middleware.cors);
+
+const config = require("./config")[env || "development"];
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -31,3 +34,5 @@ app.use("", routes);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 })
+
+module.exports = app;
