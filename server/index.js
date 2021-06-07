e6ef8dc1add04020d7d37ea83a03c9c93d80d16b
taojiang98 <jerrymchs@gmail.com>
2021-06-07 06:46:00 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const mongoose = require('mongoose');
 
-// require('dotenv').config();
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
-
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8081;
+console.log("process.env.PORT: ", process.env.PORT);
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use(cors());
 app.use(express.json());
@@ -17,17 +17,13 @@ app.use(express.json());
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
-connection.on('open', () => {
+connection.on('connected', () => {
     console.log("MongoDB database connection established successfully");
 })
 
 const routes = require("./routes");
 app.use("", routes);
 
-if (process.env.NODE_ENV === 'production') {
-    console.log("hi");
-    app.use(express.static('client/build'));
-}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
