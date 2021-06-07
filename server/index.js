06c463618a7a01182858e1a03a38c712d3ff1b0e
taojiang98 <jerrymchs@gmail.com>
2021-06-07 06:53:02 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8081;
+const port = 8081;
 console.log("process.env.PORT: ", process.env.PORT);
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://taojiang:oiw3998s@cluster0.xl2yp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.on('connected', () => {
     console.log("MongoDB database connection established successfully");
